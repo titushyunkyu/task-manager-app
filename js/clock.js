162503585d49f382a75be3b3f47d98f7dece3312
titushyunkyu <154655165+titushyunkyu@users.noreply.github.com>
2024-03-24 11:15:36 -0400

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,24 @@ function getTime(){
     const date = new Date();
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2,"0");
-    dates.innerText = `${date.getMonth()+1}/${date.getDate()}`
+    let day = new Date().getDay();
+    let dayName;
+    if (day === 0) {
+        dayName = "Sunday";
+    } else if (day === 1) {
+        dayName = "Monday";
+    } else if (day === 2) {
+        dayName = "Tuesday";
+    } else if (day === 3) {
+        dayName = "Wednesday";
+    } else if (day === 4) {
+        dayName = "Thursday";
+    } else if (day === 5) {
+        dayName = "Friday";
+    } else if (day === 6) {
+        dayName = "Saturday";
+    }
+    dates.innerText = `${date.getMonth()+1}/${date.getDate()}, ${dayName}`
     clock.innerText = `${hours}:${minutes}`
 }
 
